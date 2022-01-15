3cf5217b8b1e411ca72e8ad5f2c454700bfdb4d6
Isaac Vallee <i.vallee@astoundcommerce.com>
2022-01-15 14:44:44 -0800

    Handle letter changes and initial word state

--- src/Tile.svelte
+++ src/Tile.svelte
@@ -1,14 +1,24 @@
 <script>
-	export let value = 'L'
+  import { createEventDispatcher } from 'svelte';
+  export let value = '';
+  export let key; 
+
+  // prop: validity
+
+  const dispatch = createEventDispatcher();
 </script>
 
-<input type="text" {value}/>
+<input type="text"
+  maxlength="1"
+  bind:value
+  on:keypress={() => dispatch('keypress', {key, value})}
+/>
 
 <style>
-	input {
-		height: 100%;
-		width: 100%;
-		text-align: center;
-		border-radius: 5px;
-	}
+  input {
+    height: 100%;
+    width: 100%;
+    text-align: center;
+    border-radius: 5px;
+  }
 </style>
--- src/Word.svelte
+++ src/Word.svelte
@@ -1,17 +1,23 @@
 <script>
-	import Tile from './Tile.svelte';
-	let word = 'Testy';
-	let wordArray = word.split('');
+  import { getContext } from 'svelte';
+  import Tile from './Tile.svelte';
+  const answer = getContext('answer');
+
+  let word = answer.split('').map(() => '');
+
+  function updateWord(e) {
+    console.log(e.detail)
+    // word = [...word, e.detail];
+    // console.log(word)
+  }
+
+  // TODO
+  // 1. Collect tile states into word
+  // 2. handle active input state
+  // 
 </script>
 
-{#each wordArray as letter}
-	<Tile value={letter}/>
+
+{#each Array(answer.length) as _, i}
+  <Tile key={i} on:keypress={updateWord}/>
 {/each}
-
-
-
-<!-- <Tile/>
-<Tile/>
-<Tile/>
-<Tile/>
-<Tile/> -->
